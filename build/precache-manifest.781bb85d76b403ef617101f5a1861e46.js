02d73fa0f72dbbf4e4894a74344b1e6fa6b9a740
Micheal A <aad3rinto@gmail.com>
2022-10-13 14:25:50 +0000

    handle complex change

---
+++ build/precache-manifest.781bb85d76b403ef617101f5a1861e46.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5244478e8c9adb4d86f236fd6ab757b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebaa48b2cdb908980174",
+    "url": "/static/js/2.cc615f24.chunk.js"
+  },
+  {
+    "revision": "0b5b224e51e41a0fa042",
+    "url": "/static/js/main.f8a2205c.chunk.js"
+  },
+  {
+    "revision": "e76038f518559c30373e",
+    "url": "/static/js/runtime-main.2157d5e8.js"
+  }
+]);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function App() {
+  let [lastName, setLastName] = useState("");
+  let [firstName, setfirstName] = useState("");
+ 
+
+  function handleChange(event){
+     firstName = event.target.value;
+   
+
+  };
+
+  function handleChange2(event){
+    lastName = event.target.value;
+
+  };
+
+  function handleClick(){
+    
+    setfirstName(firstName)
+    setLastName(lastName)
+   
+  };
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {firstName} {lastName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange2} name="lName" placeholder="Last Name" />
+        <button onClick={handleClick}>Submit</button>
       </form>
     </div>
   );
